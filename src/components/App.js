52fc5b4b77ab975d83a02cdbcd07b96a632bbd4d
bben95 <benakabp@gmail.com>
2023-03-24 15:46:01 +0530

    theme changed

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
 
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   return (
-    <div style={isDarkMode?{backgroundColor:"rgb(255, 255, 255)"}:{backgroundColor:"rgb(0, 0, 0)"}} id="main">
-      <h1 style={isDarkMode?{color:"rgb(0, 0, 0)"}:{color:"rgb(255, 255, 255)"}}>{isDarkMode? "Light Mode Activated":"Dark Mode Activated"}</h1>
-      <button style={isDarkMode?{}:{backgroundColor:"rgb(255, 255, 255)"}} onClick={()=>{setIsDarkMode(!isDarkMode)}}>{isDarkMode?"Switch to Dark Mode":"Switch to Light Mode"}</button>
+    <div style={isDarkMode?{backgroundColor:"rgb(0, 0, 0)"}:{backgroundColor:"rgb(255, 255, 255)"}} id="main">
+      <h1 style={isDarkMode?{color:"rgb(255, 255, 255)"}:{color:"rgb(0, 0, 0)"}}>{isDarkMode? "Light Mode Activated":"Dark Mode Activated"}</h1>
+      <button style={isDarkMode?{backgroundColor:"rgb(255, 255, 255)"}:{}} onClick={()=>{setIsDarkMode(!isDarkMode)}}>{isDarkMode?"Switch to Dark Mode":"Switch to Light Mode"}</button>
     </div>
   )
 }
