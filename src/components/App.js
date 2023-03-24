aeb608a4b91e9f46c1c2e708f2702a35330d5453
bben95 <benakabp@gmail.com>
2023-03-24 15:49:04 +0530

    final change

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,8 @@ const App = () => {
 
   return (
     <div style={isDarkMode?{backgroundColor:"rgb(0, 0, 0)"}:{backgroundColor:"rgb(255, 255, 255)"}} id="main">
-      <h1 style={isDarkMode?{color:"rgb(255, 255, 255)"}:{color:"rgb(0, 0, 0)"}}>{isDarkMode? "Light Mode Activated":"Dark Mode Activated"}</h1>
-      <button style={isDarkMode?{backgroundColor:"rgb(255, 255, 255)"}:{}} onClick={()=>{setIsDarkMode(!isDarkMode)}}>{isDarkMode?"Switch to Dark Mode":"Switch to Light Mode"}</button>
+      <h1 style={isDarkMode?{color:"rgb(255, 255, 255)"}:{color:"rgb(0, 0, 0)"}}>{isDarkMode?"Dark Mode Activated":"Light Mode Activated"}</h1>
+      <button style={isDarkMode?{backgroundColor:"rgb(255, 255, 255)"}:{}} onClick={()=>{setIsDarkMode(!isDarkMode)}}>{isDarkMode?"Switch to Light Mode":"Switch to Dark Mode"}</button>
     </div>
   )
 }
